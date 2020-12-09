007fad3185ae160816118d915607cdc644b4b6b0
jae-yong-2 <ljy8972@naver.com>
2020-12-10 05:17:02 +0900

    changes in click.py

--- click.py
+++ click.py
@@ -5,8 +5,8 @@ from PIL import ImageGrab
 import numpy as np
 
 table = 0
-point = []
 max_count = 0
+point = []
 color = []
 
 
@@ -14,14 +14,15 @@ color = []
 
 
 def mouse_callback(event, y, x, flags, param):
-    global max_count
+    global max_count,table
     if mouse.is_pressed("left"):
         point.append([x, y])
         color.append(param[x][y])
         print(x, y)
         max_count = max_count + 1
         time.sleep(0.1)
-
+    if max_count==table:
+        cv.destroyAllWindows()
 
 def call_img():
     img = ImageGrab.grab()
@@ -29,19 +30,23 @@ def call_img():
 
 
 def click_img(table_count):
-    global max_count
+    global max_count, table,point,color
+    point = []
+    color = []
     img = ImageGrab.grab()
     img = cv.cvtColor(np.array(img), cv.COLOR_BGR2GRAY)
     table = table_count
     cv.namedWindow('image')  # 마우스 이벤트 영역 윈도우 생성
     cv.setMouseCallback('image', mouse_callback, param=img)
 
+    cv.imshow('image', img)
     while True:
-        cv.imshow('image', img)
         if max_count == table:
             max_count = 0
             break
         k = cv.waitKey(1) & 0xFF
+    max_count=0
+    table=0
     cv.destroyAllWindows()
 
     return point, color[0]
