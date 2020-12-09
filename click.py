4f6f1f9bc488ec28f8135d71e8b49590c7cbcf87
jae-yong-2 <49151220+jae-yong-2@users.noreply.github.com>
2020-12-09 23:31:26 +0900

    click

--- click.py
+++ click.py
@@ -1,10 +1,10 @@
 import cv2 as cv  # OpenCV import
 import mouse
 import time
+from PIL import ImageGrab
 
 table = 0
 point = []
-max_count = 0
 
 
 # 마우스 이벤트 콜백함수 정의
@@ -23,23 +23,19 @@ def call_img(img_):
     return img_
 
 
-def click_img(img_source):
-    global max_count
-    img = call_img(img_source)
-    table = int(input("테이블 수를 입력하세요. : "))
-    cv.namedWindow('image')  # 마우스 이벤트 영역 윈도우 생성
-    cv.setMouseCallback('image', mouse_callback, param=img)
+def click_img(table_count):
+    max_count = 0
+    img = ImageGrab.grab()
+    table = table_count
+    cv.namedWindow('image_click')  # 마우스 이벤트 영역 윈도우 생성
+    cv.setMouseCallback('image_click', mouse_callback, param=img)
+
     while True:
-        cv.imshow('image', img)
+        cv.imshow('image_click', img)
         if max_count == table:
             max_count = 0
             break
         k = cv.waitKey(1) & 0xFF
     cv.destroyAllWindows()
-
-    image = img.copy()
-    for point_ in point:
-        cv.circle(image, (point_[1], point_[0]), 2, (0, 255, 255), thickness=-1)
-    cv.imshow("image", image)
 
     return point
